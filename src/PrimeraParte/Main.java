a940594166b580d92362b583fb3b0bebd6e98859
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-03-26 15:28:10 -0500

    [.] Ejercicios - Tema 3: Inicializa Proyecto

---
+++ src/PrimeraParte/Main.java
@@ -0,0 +1,11 @@
+package PrimeraParte;
+
+/** Primera parte:
+- Crear una función con tres parámetros que sean números que se suman entre sí.
+- Llamar a la función en el main y darle valores. */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println( "Primera Parte" );
+    }
+}
